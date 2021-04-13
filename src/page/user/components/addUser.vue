038a91e1cd0ce845f03bf37ce91905895254d89f
zengwqiang <zengwqiang@126.com>
2021-04-14 00:27:44 +0800

    option渲染

--- src/page/user/components/addUser.vue
+++ src/page/user/components/addUser.vue
@@ -3,29 +3,29 @@
         <div class="add">
             <el-form ref="form" :model="formData" label-width="80px" :rules="rules">
                 <el-form-item label="姓名" prop="userName">
-                    <el-input v-model="formData.userName"></el-input>
+                    <el-input v-model="formData.userName" clearable></el-input>
                 </el-form-item>
                 <el-form-item label="登录账号" prop="loginName">
                     <el-input v-model="formData.loginName" v-if="formData.id" disabled></el-input>
-                    <el-input v-model="formData.loginName" v-else placeholder="请输入字母或数字账号" maxlength="18" show-word-limit></el-input>
+                    <el-input v-model="formData.loginName" v-else placeholder="请输入字母或数字账号" maxlength="18" show-word-limit clearable></el-input>
                 </el-form-item>
                 <el-form-item label="密码" prop="password">
-                    <el-input v-model="formData.password" type="password"></el-input>
+                    <el-input v-model="formData.password" type="password" clearable></el-input>
                 </el-form-item>
                 <el-form-item label="确认密码" prop="rePassword">
-                    <el-input v-model="formData.rePassword" type="password" placeholder="确认2次密码一致"></el-input>
+                    <el-input v-model="formData.rePassword" type="password" placeholder="确认2次密码一致" clearable></el-input>
                 </el-form-item>
                 <el-form-item label="电话" prop="userMobile">
-                    <el-input v-model="formData.userMobile" placeholder="请输入11位手机号" maxlength="11" show-word-limit></el-input>
+                    <el-input v-model="formData.userMobile" placeholder="请输入11位手机号" maxlength="11" show-word-limit clearable></el-input>
                 </el-form-item>
                 <el-form-item label="性别" >
                     <el-select v-model="formData.userSex" placeholder="请选择性别">
-                        <el-option v-for="(item,index) in sexEnum" :key="index" :value="item.value" :label="item.label"></el-option>
+                        <el-option v-for="(item,index) in sexEnum" :key="item.value" :value="item.value" :label="item.label"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="门店">
                     <el-select v-model="formData.deptNo" placeholder="请选择门店">
-<!--                        <el-option :v-for="(value,key) in deptEnum" :key="key" :value="value" :label="value"></el-option>-->
+                        <el-option v-for="(value,key) in deptOption" :key="value" :value="key" :label="value"></el-option>
                     </el-select>
                 </el-form-item>
             </el-form>
@@ -52,6 +52,12 @@
             }
         },
         data() {
+            const  checkPassword = (rule, value, callback) => {
+                if (value === this.formData.password) {
+                    return callback()
+                }
+                callback(new Error('2次密码不一致'))
+            }
             return {
                 dialogVisible: true,
                 formData: {
@@ -66,20 +72,22 @@
                 },
                 title:'添加人员',
                 sexEnum:[{label:"男",value:1},{label:"女",value:2}],
-                deptEnum:{},
                 rules: {
                     userName: [{required: true, message: "请输入姓名", trigger: 'blur'}],
                     loginName: [{required: true, message: "请输入登录名（英文或数字）", trigger: 'blur'}],
-                    userMobile:[{required:true,message:"请输入手机号",trigger:'blur'}]
+                    userMobile:[{required:true,message:"请输入手机号",trigger:'blur'}],
+                    password:[{required:true,message:"请输入密码",trigger:'blur'}],
+                    rePassword:[{required:true,message:"请输入确认密码",trigger:'blur'},{validator:checkPassword,trigger:'blur'}]
                 }
             }
+
+
         },
         created() {
             this.formData=Object.assign({},this.formData,this.rowData)
             if(this.formData.id){
                 this.title='修改人员'
             }
-            this.deptEnum=Object.assign({},this.deptEnum,this.deptOption)
         },
         methods: {
             hideDialog() {
